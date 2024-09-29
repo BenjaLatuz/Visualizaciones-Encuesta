08527c1d5be18744d4690dd578f0584181a704d3
Benjamin <benjamin.latuz@ug.uchile.cl>
2024-09-29 19:38:26 -0300

    Terminado vista para ver tecnologías más demandadas, faltan agregar más categorías.

--- app.py
+++ app.py
@@ -49,6 +49,21 @@ html, body, [class*="ViewContainer"] {
 </style>
 """, unsafe_allow_html=True)
 
+# Define el color de fondo y el color del texto para que coincida con el estilo de tu página
+background_color = '#1c1f26'  # Color de fondo de la página
+text_color = '#ddd'  # Color del texto en la página
+edge_color = '#555'  # Color para los bordes del gráfico, que podría coincidir con tu estilo
+
+# Configuración de Matplotlib para que coincida con los estilos de Streamlit
+plt.rcParams['figure.facecolor'] = background_color
+plt.rcParams['axes.facecolor'] = background_color
+plt.rcParams['text.color'] = text_color
+plt.rcParams['axes.labelcolor'] = text_color
+plt.rcParams['xtick.color'] = text_color
+plt.rcParams['ytick.color'] = text_color
+plt.rcParams['axes.edgecolor'] = edge_color
+
+
 # Cargar el archivo CSV
 df = pd.read_csv('Encuesta.csv')
 
@@ -70,49 +85,105 @@ with tab1:
     st.write("A continuación se muestran las visualizaciones asociadas a las respuestas de la encuesta en el área de Software")
     
     # Selector de categoría
+    # Se buscará que si hay muchas tecnologías asociadas, haya un scroll horizontal
+    # y proximamente, se ligará a un csv y así se actualizará automáticamente (no hardcodeado, si es que alcanza el tiempo)
+    # Datos de categorías de tecnologías
     category_options = {
-        "Lenguajes de Programación (Desarrollo de Software)": [
-            {"name": "JavaScript", "percentage": "38%", "frequency": "131 veces mencionado en 350 ofertas"},
-            {"name": "Java", "percentage": "36%", "frequency": "125 veces mencionado en 350 ofertas"},
-            {"name": "Python", "percentage": "28%", "frequency": "97 veces mencionado en 350 ofertas"},
-            {"name": "TypeScript", "percentage": "17%", "frequency": "58 veces mencionado en 350 ofertas"}
+        "Lenguajes de Programación": [
+            {"name": "Python (Ingeniería de Datos)", "percentage": "64%", "frequency": "147 veces mencionado en 229 ofertas de Ingeniería de Datos"},
+            {"name": "SQL", "percentage": "43%", "frequency": "275 veces mencionado en 640 ofertas totales"},
+            {"name": "JavaScript", "percentage": "38%", "frequency": "131 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Java", "percentage": "36%", "frequency": "125 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Python (Ingeniería de Software)", "percentage": "28%", "frequency": "97 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "R", "percentage": "21%", "frequency": "49 veces mencionado en 229 ofertas de Ingeniería de Datos"},
+            {"name": "TypeScript", "percentage": "17%", "frequency": "58 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "C", "percentage": "12%", "frequency": "40 veces mencionado en 345 ofertas de Ingeniería de Software"}
         ],
         "Tecnologías Frontend": [
-            {"name": "React", "percentage": "34%", "frequency": "118 veces mencionado en 350 ofertas"},
-            {"name": "Angular", "percentage": "28%", "frequency": "97 veces mencionado en 350 ofertas"},
-            {"name": "HTML", "percentage": "24%", "frequency": "82 veces mencionado en 350 ofertas"},
-            {"name": "CSS", "percentage": "22%", "frequency": "77 veces mencionado en 350 ofertas"},
-            {"name": "Vue", "percentage": "9%", "frequency": "32 veces mencionado en 350 ofertas"}
+            {"name": "React", "percentage": "34%", "frequency": "118 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Angular", "percentage": "28%", "frequency": "97 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "HTML", "percentage": "24%", "frequency": "82 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "CSS", "percentage": "22%", "frequency": "77 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Vue", "percentage": "9%", "frequency": "32 veces mencionado en 345 ofertas de Ingeniería de Software"}
         ],
         "Tecnologías Backend": [
-            {"name": "Spring Boot", "percentage": "85%", "frequency": "1200 times"},
-            {"name": "Django", "percentage": "75%", "frequency": "1100 times"},
-            {"name": "Rails", "percentage": "65%", "frequency": "900 times"}
+            {"name": "Spring Boot", "percentage": "17%", "frequency": "118 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Rails", "percentage": "4%", "frequency": "15 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Express.js", "percentage": "4%", "frequency": "15 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "NestJS", "percentage": "4%", "frequency": "15 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Next.js", "percentage": "4%", "frequency": "14 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Django", "percentage": "4%", "frequency": "12 veces mencionado en 345 ofertas de Ingeniería de Software"}
         ],
-        "Otras Herramientas o Tecnologías de Desarrollo de Software": [
-            {"name": "Git", "percentage": "85%", "frequency": "1200 times"},
-            {"name": "Docker", "percentage": "75%", "frequency": "1100 times"},
-            {"name": "Kubernetes", "percentage": "75%", "frequency": "1100 times"},
-            {"name": "Metodología Scrum", "percentage": "75%", "frequency": "1100 times"}
+        "Otras Herramientas, Tecnologías, Metodologías y/o Prácticas de Desarrollo de Software": [
+            {"name": "Git", "percentage": "39%", "frequency": "134 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Arquitectura Rest", "percentage": "19%", "frequency": "65 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Docker", "percentage": "16%", "frequency": "54 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Arquitectura Microservicios", "percentage": "14%", "frequency": "50 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Metodología Scrum", "percentage": "14%", "frequency": "48 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Kubernetes", "percentage": "11%", "frequency": "39 veces mencionado en 345 ofertas de Ingeniería de Software"},
+            {"name": "Pruebas Unitarias", "percentage": "10%", "frequency": "33 veces mencionado en 345 ofertas de Ingeniería de Software"}
+        ],
+        "Herramientas y Servicios en la Nube": [
+            {"name": "AWS", "percentage": "26%", "frequency": "169 veces mencionado en 640 ofertas totales"},
+            {"name": "Azure", "percentage": "17%", "frequency": "111 veces mencionado en 640 ofertas totales"},
+            {"name": "Google Cloud Platform", "percentage": "13%", "frequency": "84 veces mencionado en 640 ofertas totales"}
+            
         ]
     }
+    # Crear un estado para la página actual y la categoría seleccionada
+    if 'current_page' not in st.session_state:
+        st.session_state.current_page = 0
 
+    if 'previous_category' not in st.session_state:
+        st.session_state.previous_category = None
+
+    # Seleccionar la categoría principal
     selected_category = st.selectbox("Seleccione una categoría para ver los detalles:", list(category_options.keys()))
 
+    # Restablecer la página a 1 si cambia la categoría
+    if st.session_state.previous_category != selected_category:
+        st.session_state.current_page = 0  # Restablece a la primera página cuando cambia de categoría
+        st.session_state.previous_category = selected_category  # Actualiza la categoría previa
+
+    # Obtener las tecnologías de la categoría seleccionada
     technologies = category_options[selected_category]
 
-    cols = st.columns(len(technologies))
-    
-    for col, tech in zip(cols, technologies):
-        col.markdown(f"""
-        <div class="card">
-            <div class="metric-name">{tech['name']}</div>
-            <div class="metric-value">{tech['percentage']}</div>
-            <div class="metric-freq">{tech['frequency']}</div>
-        </div>
-        """, unsafe_allow_html=True)
+    # Definir cuántas tecnologías mostrar por página
+    num_items_per_page = 4
+    total_items = len(technologies)
+    num_pages = (total_items + num_items_per_page - 1) // num_items_per_page  # Número total de páginas
 
-    ########################## FRONTEND ##################################################
+    # Botones para avanzar y retroceder páginas
+    col1, col2, col3 = st.columns([1, 8, 1])
+
+    with col1:
+        if st.button("Anterior") and st.session_state.current_page > 0:
+            st.session_state.current_page -= 1
+
+    with col3:
+        if st.button("Siguiente") and st.session_state.current_page < num_pages - 1:
+            st.session_state.current_page += 1
+
+    # Calcular el índice de inicio y final para mostrar las tecnologías de la página actual
+    start_idx = st.session_state.current_page * num_items_per_page
+    end_idx = min(start_idx + num_items_per_page, total_items)
+
+    # Mostrar solo las tecnologías de la página actual
+    cols = st.columns(num_items_per_page)
+    for idx, col in enumerate(cols):
+        if start_idx + idx < len(technologies):
+            tech = technologies[start_idx + idx]
+            col.markdown(f"""
+            <div class="card">
+                <div class="metric-name">{tech['name']}</div>
+                <div class="metric-value">{tech['percentage']}</div>
+                <div class="metric-freq">{tech['frequency']}</div>
+            </div>
+            """, unsafe_allow_html=True)
+
+    # Mostrar el número de página actual
+    st.write(f"Página {st.session_state.current_page + 1} de {num_pages}")
+        ########################## FRONTEND ##################################################
 
     preguntas_seleccionadas = ['React', 'Vue', 'Angular', 'HTML y CSS']
 
